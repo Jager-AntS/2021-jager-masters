a3e81c0c2da7f68b10f914af298e2c28c1b04ae7
Ant S <a.seryakov@reply.com>
2021-03-06 12:47:00 +0300

    Initial commit

---
+++ code/Pipelines/GetFieldSerializer/GetMultilistFieldSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreHackathon2021.Pipelines.GetFieldSerializer
+{
+    public class GetMultilistFieldSerializer
+    {
+    }
+}
